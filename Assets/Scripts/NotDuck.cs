a73b78525569f1db9c65a7a517b9193514519d0d
Bhatck-Aegol <aleixoguilherme2009@gmail.com>
2025-07-18 11:13:37 -0300

    feat: Jumping

---
+++ Assets/Scripts/NotDuck.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotDuck : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool jumping;
+    [SerializeField] float jumpForce;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumping) 
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jumping = false;
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
+    }
+}
